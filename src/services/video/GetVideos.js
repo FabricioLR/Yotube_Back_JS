97e381dbdf24c40ee03698ab23ea2084abfd5568
FabricioLR <dimamitoyt@gmail.com>
2022-02-05 19:56:34 -0300

    atualizacao getvideos

--- src/services/video/GetVideos.js
+++ src/services/video/GetVideos.js
@@ -1,7 +1,11 @@
 const Video = require("../../models/Video")
 
 async function GetVideos(){
-    return await Video.findAll()
+    return await Video.findAll({
+        include: {
+            association: "users"
+        }
+    })
 }
 
 module.exports = GetVideos
